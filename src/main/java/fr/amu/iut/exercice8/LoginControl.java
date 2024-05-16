45e334b8b6429b0cbdcda608bd95cbcbc55f4bb6
Thomas <tran.thomas.thien@gmail.com>
2024-05-16 11:59:50 +0200

    feat : Exercice 8

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,29 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
+    private String mdp= "";
+    @FXML
+    private TextField utilisateur;
+    @FXML
+    private PasswordField pwd;
 
+    @FXML
     private void okClicked() {
-    }
+        System.out.print(utilisateur.getText());
 
+        for (int i = 0 ; i<pwd.getText().length(); ++i){
+            mdp += "*";
+        }
+        System.out.println(" " + mdp);
+    }
+    @FXML
     private void cancelClicked() {
+        utilisateur.clear();
+        pwd.clear();
     }
 }
