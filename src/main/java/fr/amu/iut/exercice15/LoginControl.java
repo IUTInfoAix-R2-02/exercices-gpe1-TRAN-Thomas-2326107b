f85cf396582bb3cb082ad2f9e93533f604b74ccf
Thomas <tran.thomas.thien@gmail.com>
2024-05-23 17:32:11 +0200

    feat : debut Exercice 15

--- src/main/java/fr/amu/iut/exercice15/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice15/LoginControl.java
@@ -1,5 +1,10 @@
-package fr.amu.iut.exercice5;
+package fr.amu.iut.exercice15;
 
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -12,8 +17,31 @@ public class LoginControl extends GridPane {
     @FXML
     private PasswordField pwd;
 
+    @FXML
+    public void initialize(){
+        createBindings();
+    }
+
+    private static BooleanBinding id6Carac;
+    private static BooleanProperty bon;
+
     private void createBindings() {
-        // MÉTHODE À COMPLÉTER
+        bon = new SimpleBooleanProperty();
+
+        id6Carac = new BooleanBinding() {
+            {
+                this.bind(userId.textProperty());
+            }
+            @Override
+            protected boolean computeValue() {
+                if (userId.getLength() < 6){
+                    return false;
+                }
+                return true;
+            }
+        };
+        bon.bind(id6Carac);
+        pwd.editableProperty().bind(bon);
     }
 
     @FXML
